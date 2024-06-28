704afa3773373bc5aa4d29ca425e2d16e51d5e14
Zakaria Ismail <1112zakaria@gmail.com>
2024-06-27 22:29:24 -0400

    calibrate.sh output enhancements

--- scripts/calibrate.sh
+++ scripts/calibrate.sh
@@ -13,8 +13,10 @@ cd ../dji-firmware-tools
 echo "Start of calibration shell script"
 echo "Calibrating gimbal - JointCoarse..."
 python comm_og_service_tool.py --port $1 $2 GimbalCalib JointCoarse
-sleep 25
+echo "Sleeping for 20 seconds to let the JointCoarse calibration finish..."
+sleep 20
 echo "Calibrating gimbal - LinearHall..."
 python comm_og_service_tool.py --port $1 $2 GimbalCalib LinearHall
-sleep 40
+echo "Sleeping for 35 seconds to let the LinearHall calibration finish..."
+sleep 35
 echo "Calibration is complete!"
