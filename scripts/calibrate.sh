63595eb6f74e9009a1b4cba4aff998f0679f14b2
Zakaria Ismail <1112zakaria@gmail.com>
2024-06-27 22:26:47 -0400

    enhancements to calibrate.sh

--- scripts/calibrate.sh
+++ scripts/calibrate.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env bash
 
 # $1: port
-# $2: baudrate
+# $2: drone version
 
 # Run the following commands to calibrate gimbal
-set -e
+# FIXME: call to comm_og_service_tool.py returns error value on shell and outputs:
+#   Error: Unrecognized response to calibration command JointCoarse request.
+# The script appears to perform calibration regardless...
+
+# set -e    # commented because calls to comm_og_service_tool.py return error code
 cd ../dji-firmware-tools
+echo "Start of calibration shell script"
+echo "Calibrating gimbal - JointCoarse..."
 python comm_og_service_tool.py --port $1 $2 GimbalCalib JointCoarse
+sleep 25
+echo "Calibrating gimbal - LinearHall..."
 python comm_og_service_tool.py --port $1 $2 GimbalCalib LinearHall
+sleep 40
+echo "Calibration is complete!"
